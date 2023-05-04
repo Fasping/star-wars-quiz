727cabfc3b20404ea28d9bd23d64a4b1d70c0bf4
Fernando Cases <nandocasesgarcia@gmail.com>
2023-05-04 15:29:43 +0200

    Feat: add css for ui

--- quiz-star-wars/src/components/Quiz.jsx
+++ quiz-star-wars/src/components/Quiz.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 import questions from "../questions.json";
+import "./Quiz.css";
 
 function Quiz() {
-  const [level, setLevel] = useState("easy");
+  const [level, setLevel] = useState("");
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
+
+  const handleLevelSelection = (selectedLevel) => {
+    setLevel(selectedLevel);
+    setCurrentQuestion(0);
+    setScore(0);
+  };
 
   const handleOptionClick = (selectedOption) => {
     const isCorrect =
@@ -12,64 +19,68 @@ function Quiz() {
     if (isCorrect) {
       setScore(score + 1);
     }
+
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < questions[level].length) {
       setCurrentQuestion(nextQuestion);
     } else {
       alert(`Your score is ${score}/${questions[level].length}`);
-      setCurrentQuestion(0);
-      setScore(0);
+      setLevel("");
     }
   };
 
   return (
-    <div>
-      <h1>Star Wars Quiz</h1>
-      <div>
-        <label>
-          <input
-            type="radio"
-            name="level"
-            value="easy"
-            checked={level === "easy"}
-            onChange={() => setLevel("easy")}
-          />
-          Easy
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="level"
-            value="medium"
-            checked={level === "medium"}
-            onChange={() => setLevel("medium")}
-          />
-          Medium
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="level"
-            value="hard"
-            checked={level === "hard"}
-            onChange={() => setLevel("hard")}
-          />
-          Hard
-        </label>
-      </div>
-      <div>
-        <p>
-          Question {currentQuestion + 1}/{questions[level].length}
-        </p>
-        <h2>{questions[level][currentQuestion].question}</h2>
-        <ul>
-          {questions[level][currentQuestion].options.map((option) => (
-            <li key={option} onClick={() => handleOptionClick(option)}>
-              {option}
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className="quiz-container">
+      {level === "" ? (
+        <>
+          <h1 className="quiz-title">Star Wars Quiz</h1>
+          <div className="quiz-level-container">
+            <button
+              className="quiz-level easy"
+              onClick={() => handleLevelSelection("easy")}
+            >
+              Easy
+            </button>
+            <button
+              className="quiz-level medium"
+              onClick={() => handleLevelSelection("medium")}
+            >
+              Medium
+            </button>
+            <button
+              className="quiz-level hard"
+              onClick={() => handleLevelSelection("hard")}
+            >
+              Hard
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="quiz-question-container">
+            <p className="quiz-question-counter">
+              Question {currentQuestion + 1}/{questions[level].length}
+            </p>
+            <h2 className="quiz-question">
+              {questions[level][currentQuestion].question}
+            </h2>
+            <ul className="quiz-options">
+              {questions[level][currentQuestion].options.map((option) => (
+                <li
+                  key={option}
+                  className="quiz-option"
+                  onClick={() => handleOptionClick(option)}
+                >
+                  {option}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="quiz-score-container">
+            <p className="quiz-score">Score: {score}</p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
